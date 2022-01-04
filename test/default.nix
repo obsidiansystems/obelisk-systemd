8cefc70ae7c13ba93077da35c93f6cb7f303500e
Ali Abrar <ali.abrar@obsidian.systems>
2022-01-04 18:27:04 -0500

    tests: wait for server to start before curl-ing

--- test/default.nix
+++ test/default.nix
@@ -11,6 +11,7 @@ import (thunkSource ./nixpkgs + "/nixos/tests/make-test-python.nix") ({ pkgs, ..
     start_all()
     machine.wait_for_unit("default.target")
     machine.wait_for_unit("skeleton")
+    machine.wait_for_open_port("8080")
     machine.execute('logger -t TEST "#1 Verifying that server is running..."')
     machine.execute('logger -t TEST $(systemctl status skeleton.service)')
     machine.succeed("curl localhost:8080 | grep \"This string comes from config/common/example\"")
